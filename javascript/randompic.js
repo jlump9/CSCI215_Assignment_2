4874d244085deed61c56365104e371d50b7b209c
Josh <lumpkinjt@g.cofc.edu>
2018-10-20 14:45:12 -0400

    Initial Commit

---
+++ javascript/randompic.js
@@ -0,0 +1,9 @@
+
+
+var randomPic = [];
+
+randomPic[0] = "images/yam.jpg";
+randomPic[1] = "images/coins.jpeg";
+randomPic[2] = "images/cow.jpg";
+randomPic[3] = "images/pineapple.jpeg";
+randomPic[4] = "images/bike.jpg";
