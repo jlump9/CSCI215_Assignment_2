1544cc95b73a9c9f53b661496ead9128b66e7427
Josh <lumpkinjt@g.cofc.edu>
2018-10-20 14:53:34 -0400

    added Var

--- javascript/randompic.js
+++ javascript/randompic.js
@@ -7,3 +7,5 @@ randomPic[1] = "images/coins.jpeg";
 randomPic[2] = "images/cow.jpg";
 randomPic[3] = "images/pineapple.jpeg";
 randomPic[4] = "images/bike.jpg";
+
+var randomChoice = (math.floor(math.random(randomPic)))
